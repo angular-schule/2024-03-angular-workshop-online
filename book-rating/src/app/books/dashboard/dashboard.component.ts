16cb8cff2a01ead7a6a33625537f9e8aed1f58a3
Ferdinand Malcher <ferdinand@malcher.media>
2024-03-06 14:15:40 +0100

    Buchliste mit Loop Control Flow

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { Book } from '../shared/book';
+import { BookComponent } from '../book/book.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [BookComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
+  books: Book[] = [];
 
+  constructor() {
+    this.books = [
+      {
+        isbn: '123',
+        title: 'Angular',
+        description: 'Das große Praxisbuch',
+        price: 42.9,
+        rating: 5,
+      },
+      {
+        isbn: '456',
+        title: 'Vue.js',
+        description: 'Das grüne Framework',
+        price: 36.9,
+        rating: 3,
+      },
+    ];
+  }
 }
---
+++ book-rating/src/app/books/shared/book.ts
@@ -0,0 +1,8 @@
+export interface Book {
+  isbn: string; // Primary Key
+  title: string;
+  // authors: string[];
+  description: string;
+  rating: number;
+  price: number;
+}
