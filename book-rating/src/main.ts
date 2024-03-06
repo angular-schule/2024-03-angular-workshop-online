cf6c3ff5519b199da9b6f38b4395bc39b0885d3f
Ferdinand Malcher <ferdinand@malcher.media>
2024-03-06 10:23:47 +0100

    TypeScript

--- book-rating/src/main.ts
+++ book-rating/src/main.ts
@@ -4,3 +4,47 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+/////////////
+
+
+
+
+export class Customer {
+  /*id: number;
+
+  constructor(id: number) {
+    this.id = id;
+  }*/
+
+  constructor(public id: number) {}
+
+  fooBar(arg: string): number {
+    setTimeout(() => {
+      console.log('HALLO Customer ' + this.id);
+    }, 2000);
+
+    return 0;
+  }
+
+}
+
+
+
+
+
+
+const myCustomer = new Customer(3);
+myCustomer.fooBar('')
+
+
+////
+
+/*
+const bar = function (arg) {
+  return arg + 1;
+}
+
+const bar2 = arg => arg + 1;
+*/
