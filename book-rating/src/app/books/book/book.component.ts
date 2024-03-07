ba6f59c8fb4ee8b2081018d3ffd50ffc10a9af74
Ferdinand Malcher <ferdinand@malcher.media>
2024-03-07 11:09:29 +0100

    Events

--- book-rating/src/app/books/book/book.component.ts
+++ book-rating/src/app/books/book/book.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Book } from '../shared/book';
 import { RatingComponent } from '../rating/rating.component';
 
@@ -13,4 +13,17 @@ export class BookComponent {
   // hier dürfen Daten hineinfließen von der Elternkomponente
   // von oben nach unten
   @Input() book?: Book;
+
+  // hier fließen Daten hinaus zur Elternkomponente
+  // von unten nach oben
+  @Output() rateUp = new EventEmitter<Book>();
+  @Output() rateDown = new EventEmitter<Book>();
+
+  doRateUp() {
+    this.rateUp.emit(this.book);
+  }
+
+  doRateDown() {
+    this.rateDown.emit(this.book);
+  }
 }
