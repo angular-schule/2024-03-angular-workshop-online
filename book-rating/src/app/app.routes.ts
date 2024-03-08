7514ba5fc8c6cac922b01bbe7fafcf5892873497
Ferdinand Malcher <ferdinand@malcher.media>
2024-03-08 14:15:57 +0100

    Routen und Links

--- book-rating/src/app/app.routes.ts
+++ book-rating/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { booksRoutes } from './books/books.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Weiterleitung vom leeren Pfad: fast immer pathMatch:full notwendig
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  ...booksRoutes
+
+  // achtung: Wildcard immer ganz unten!
+  // { path: '**', component: ErrorPageComponent },
+];
