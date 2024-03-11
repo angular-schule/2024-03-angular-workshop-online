1418556049be58e18fd53aefb9736607f2b00596
Ferdinand Malcher <ferdinand@malcher.media>
2024-03-11 12:00:27 +0100

    aufräumen

--- book-rating/src/app/books/book-create/book-create.component.ts
+++ book-rating/src/app/books/book-create/book-create.component.ts
@@ -1,17 +1,12 @@
-import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-book-create',
   standalone: true,
-  imports: [FormsModule, JsonPipe],
+  imports: [],
   templateUrl: './book-create.component.html',
   styleUrl: './book-create.component.scss'
 })
 export class BookCreateComponent {
-  book = {
-    title: 'Foo',
-    isbn: '123'
-  };
+
 }
