12265ed800b4acc823436d507f83581ab3d609b2
Ferdinand Malcher <ferdinand@malcher.media>
2024-03-11 14:25:32 +0100

    Reactive Forms

--- book-rating/src/app/books/book-create/book-create.component.ts
+++ book-rating/src/app/books/book-create/book-create.component.ts
@@ -1,12 +1,64 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-book-create',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './book-create.component.html',
   styleUrl: './book-create.component.scss'
 })
 export class BookCreateComponent {
+  bookForm = new FormGroup({
+    isbn: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(13),
+        Validators.pattern(/^[0-9]*$/)
+      ]
+    }),
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required]
+    }),
+    description: new FormControl('', { nonNullable: true }),
+    rating: new FormControl(1, {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(5),
+      ]
+    }),
+    price: new FormControl(0, {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.min(0)
+      ]
+    }),
+  });
 
+  isInvalid(controlName: string): boolean {
+    const control = this.bookForm.get(controlName);
+    if (!control) {
+      return false;
+    }
+
+    return control.invalid && control.touched;
+  }
+
+  hasError(controlName: string, errorCode: string): boolean {
+    // Hat das Control "controlName" den bestimmten Fehler "errorCode"?
+    const control = this.bookForm.get(controlName);
+    if (!control) {
+      return false;
+    }
+
+    return control.hasError(errorCode) && control.touched;
+    // return control.errors?.[errorCode] // TODO
+  }
 }
