227db17197fdcc367920fcb5bfa64ffcff80adb3
Ferdinand Malcher <ferdinand@malcher.media>
2024-03-13 11:01:59 +0100

    AsyncPipe

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,32 +1,34 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { filter, map, switchMap } from 'rxjs';
+import { EMPTY, Observable, catchError, filter, map, switchMap } from 'rxjs';
 import { BookStoreService } from '../shared/book-store.service';
 import { Book } from '../shared/book';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-book-details',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe, JsonPipe],
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.scss'
 })
 export class BookDetailsComponent {
-  book?: Book;
+  book$: Observable<Book>;
 
   constructor(private route: ActivatedRoute, private bs: BookStoreService) {
     // PULL
     // const isbn = this.route.snapshot.paramMap.get('isbn') // path: 'books/:isbn'
 
     // PUSH
-
-    this.route.paramMap.pipe(
+    this.book$ = this.route.paramMap.pipe(
       map(params => params.get('isbn')!),
       // filter((isbn): isbn is string => isbn !== null), // wenn man auf Non-Null Assertion verzichten möchte
-      switchMap(isbn => this.bs.getSingle(isbn))
-    ).subscribe(b => {
-      this.book = b;
-    })
+      switchMap(isbn => this.bs.getSingle(isbn)),
+      catchError(err => {
+        // TODO
+        return EMPTY;
+      })
+    );
 
     /*combineLatest([
       this.route.paramMap,
